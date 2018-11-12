6dbd7288a768916ffd121b7ecd4b5c4afc11575b
Wen-Kai, Chang <kai0517@gmail.com>
2018-11-12 18:12:11 +0800

    Modify the comment of showpath.sh.

--- showpath.sh
+++ showpath.sh
@@ -2,7 +2,7 @@
 # Date: 2018-11-12
 # Reference: https://www.linuxjournal.com/content/remove-duplicate-path-entries
 
-# Use awk to remove duplicate entries those retain the original order.
+# Use awk to remove duplicate entries and to retain the original order of these entries.
 
 export PATH=/usr/bin:/bin:/usr/local/bin:/usr/bin:/bin
 echo -n $PATH | awk -v RS=: -v ORS=: '!($0 in ary) {ary[$0]; printf("%s%s", length(ary) > 1 ? ":" : "", $0)}'
